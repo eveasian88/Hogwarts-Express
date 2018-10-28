c4ba2cc50e90c574bf51b252fa20e89c5a64dbb9
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-27 18:48:08 -0700

    apiKey firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// initialize firebase
+  var config = {
+    apiKey: "AIzaSyBb_VvgidrYsaUud7zcbTIDCthA_EfNwPo",
+    authDomain: "demoproject-40b0e.firebaseapp.com",
+    databaseURL: "https://demoproject-40b0e.firebaseio.com",
+    projectId: "demoproject-40b0e",
+    storageBucket: "demoproject-40b0e.appspot.com",
+    messagingSenderId: "100672990588"
+  };
+  firebase.initializeApp(config);
