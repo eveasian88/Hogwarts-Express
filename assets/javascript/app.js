a450154bd2da5059273071523b8cdd46ad91306a
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-27 19:14:25 -0700

    pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,63 @@
 // initialize firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyBb_VvgidrYsaUud7zcbTIDCthA_EfNwPo",
     authDomain: "demoproject-40b0e.firebaseapp.com",
     databaseURL: "https://demoproject-40b0e.firebaseio.com",
     projectId: "demoproject-40b0e",
     storageBucket: "demoproject-40b0e.appspot.com",
     messagingSenderId: "100672990588"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// initial values
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+var currentTime = moment();
+var index = 0;
+var trainIDs = [];
+
+// show current time
+var datetime = null,
+    date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function () {
+    datetime = $('#current-status')
+    update();
+    setInterval(update, 1000);
+});
+
+  // capture button click
+
+  // grab values from text boxes
+
+  // difference between the times
+
+  // time apart
+
+  // minute until train arrives
+
+  // next train
+
+  // arrival time
+
+  // code for handling the push
+
+  // empty text input
+
+  // don't refresh the page
+
+  // firebase watcher + initial loader HINT: this code behaves similarly to .on("child_added")
+
+  // show only 25 latest entries
+
+  // HTML to reflect 
+
