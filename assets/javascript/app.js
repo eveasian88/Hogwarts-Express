11341d20b35a5c4cc028840ea2cdef453ef61470
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-30 22:26:45 -0700

    background css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -87,6 +87,8 @@ $("#submit").on("click", function (event) {
 
     alert("Your Wish Has Been Granted!");
 
+    sessionStorage.clear();
+
     // empty text input
     $("#train-name").val("");
     $("#destination").val("");
@@ -97,22 +99,21 @@ $("#submit").on("click", function (event) {
     return;
 });
 
-// delete function not finished yet
-// $(document).on("click", ".delete", function(){
-//   var confirmDelete = confirm("Are you sure you want to cancel your wish?");
+// delete function
+// //   var confirmDelete = confirm("Are you sure you want to cancel your wish?");
 
-//    if (confirmDelete){
-//     var entry = $(this).attr("data-index");
-//     database.ref().child(trainIDs[entry]).remove();
-//     location.reload();
-//    } else{
-//      return false;
-//    }
+// //    if (confirmDelete){
+// //     var entry = $(this).attr("data-index");
+// //     database.ref().child(trainIDs[entry]).remove();
+// //     location.reload();
+// //    } else{
+// //      return false;
+// //    }
 // });
 
 
 // firebase watcher + initial loader: code behaves similarly to .on("child_added")
-database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function (snapshot) {
+database.ref().orderByChild("dateAdded").limitToLast(8).on("child_added", function (snapshot) {
 
     console.log(snapshot.val());
 
@@ -123,22 +124,6 @@ database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", funct
     console.log("Next Train: " + snapshot.val().nextArrival);
     console.log("Minutes Away: " + snapshot.val().minutesAway);
 
-    
-    // change the HTML to reflect
-    // let trainRow = $("<tr>");
-    // let trainName = $("<td>").text(snapshot.val().trainName);
-    // let destination = $("<td>").text(snapshot.val().destination);
-    // let frequency = $("<td>").text(snapshot.val().frequency);
-    // let nextArrival = $("<td>").text(snapshot.val().nextArrival);
-    // let minutesAway= $("<td>").text(snapshot.val().minutesAway);
-
-    // trainRow.append(trainName);
-    // trainRow.append(destination);
-    // trainRow.append(frequency);
-    // trainRow.append(nextArrival);
-    // trainRow.append(minutesAway);
-
-    // $("#train-table").append(trainRow);
 
     $("#train-table").append("<tr><td>" + snapshot.val().trainName + "</td>" +
         "<td>" + snapshot.val().destination + "</td>" +
@@ -147,9 +132,6 @@ database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", funct
         "<td>" + snapshot.val().minutesAway + " mins until arrival" + "</td>" + "</td></tr>");
 
     index++;
-
-    // add each train's data into the table (alternate version to above)
-    // $("#train-table > tbody").append("<tr><td>" + trainName + "</tr><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + nextArrival + "</td></tr>");
 
 
     // handle the errors
